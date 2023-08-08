679f10ec9b27d640e4cd8e89a2077dc45d6875ae
Neo <ramoroka.neo@gmail.com>
2023-08-08 16:01:07 +0200

    main

--- src/Login.js
+++ src/Login.js
@@ -1,85 +1,84 @@
 import React, { useState } from 'react';
-import { View, Text, Touchable, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Alert } from 'react-native';
 import Background from './Background';
 import { Arctic, Sky } from './Constants';
 import Field from './Field';
 import Btn from './Btn';
 
-
 const Login = (props) => {
-  const [credentials, setCredentials] = useState({email: '', password: ''});
-  const[error, setError] =useState('');
+  const [credentials, setCredentials] = useState({ email: '', password: '' });
+  const [error, setError] = useState('');
 
   const handleLogin = () => {
     const validEmail = 'user@example.com';
     const validPassword = 'password';
 
-    if (credentials.email === validEmail && credentials.password === validPassword) {
+    if (!credentials.email || !credentials.password) {
+      setError('Please enter both email and password');
+    } else if (credentials.email === validEmail && credentials.password === validPassword) {
       setError('');
-      alert('Logged In');
+      Alert.alert('Logged In', 'You have successfully logged in.');
       props.navigation.navigate('AudioRecorder');
     } else {
       setError('Invalid email or password');
     }
   };
+
   return (
     <Background>
-    <View style={{ alignItems:"center", width: 460}}>
-      <Text style={{ color:"white", 
-      fontSize: 64, 
-      fontWeight: 'bold', 
-      marginVertical: 
-      10}}>
-      Login
-      </Text>
-      <View style={{ backgroundColor: "white", 
-      height: 700, width: 460, 
-      borderTopLeftRadius: 130, 
-      paddingTop: 100, 
-      alignItems: 'center' }}
-      >
-        <Text style={{fontSize: 40, color: Arctic, fontWeight: 'bold'}}>
-        Welcome Back
+      <View style={{ alignItems: 'center', width: 460 }}>
+        <Text style={{ color: 'white', fontSize: 64, fontWeight: 'bold', marginVertical: 10 }}>
+          Login
         </Text>
-        <Text style={{ color: "grey", fontSize: 19, fontWeight: "bold", marginBottom: 20 }}>Login to your account</Text>
-        <Field 
-        placeholder="Email / Username" 
-        keyboardType={"email-address"}
-        value={credentials.email}
-        onChangeText={(text) => setCredentials({ ...credentials, email: text})}
-        />
-        <Field placeholder="Password" 
-        secureTextEntry={true} 
-          value={credentials.password}
-          onChangeText={(text) => setCredentials({...credentials, password: text })}
-        />
-         {error ? <Text style={{ color: 'red' }}>{error}</Text> : null}
-        <View style={{alignItems: 'flex-end', width: '50%', paddingRight: 16, marginBottom: 100}}>
-          <Text style={{ color: Sky, fontWeight: 'bold', font: 16 }}>
-          Forgot Password ?
+        <View
+          style={{
+            backgroundColor: 'white',
+            height: 700,
+            width: 460,
+            borderTopLeftRadius: 130,
+            paddingTop: 100,
+            alignItems: 'center',
+          }}>
+          <Text style={{ fontSize: 40, color: Arctic, fontWeight: 'bold' }}>
+            Welcome Back
           </Text>
+          <Text style={{ color: 'grey', fontSize: 19, fontWeight: 'bold', marginBottom: 20 }}>
+            Login to your account
+          </Text>
+          <Field
+            placeholder="Email / Username"
+            keyboardType="email-address"
+            value={credentials.email}
+            onChangeText={(text) => setCredentials({ ...credentials, email: text })}
+          />
+          <Field
+            placeholder="Password"
+            secureTextEntry={true}
+            value={credentials.password}
+            onChangeText={(text) => setCredentials({ ...credentials, password: text })}
+          />
+          {error ? <Text style={{ color: 'red' }}>{error}</Text> : null}
+          <View style={{ alignItems: 'flex-end', width: '50%', paddingRight: 16, marginBottom: 100 }}>
+            <Text style={{ color: Sky, fontWeight: 'bold', fontSize: 16 }}>
+              Forgot Password?
+            </Text>
+          </View>
+          <Btn
+            textColor="white"
+            bgColor={Arctic}
+            btnLabel="Login"
+            Press={handleLogin}
+          />
+          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Don't have an account? </Text>
+            <TouchableOpacity onPress={() => props.navigation.navigate('Signup')}>
+              <Text style={{ color: Arctic, fontWeight: 'bold', fontSize: 16 }}>Signup</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <Btn
-        textColor='white'
-        bgColor={Arctic}
-        btnLabel="Login"
-        Press={() => {
-          alert("Logged In");
-          props.navigation.navigate('AudioRecorder');
-        }}
-      />
-        <View style={{ display: 'flex', flexDirection: 'row', justifyContent:
-        "center"}}>
-         
-          <Text style={{ fontSize: 16, fontWeight: "bold" }}>Don't have an account ?</Text>
-          <TouchableOpacity onPress={() => props.navigation.navigate("Signup")}>
-          <Text style={{ color: Arctic, fontWeight: 'bold', fontSize: 16 }}>Signup</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
       </View>
     </Background>
-  )
-}
+  );
+};
 
 export default Login;
--- src/Signup.js
+++ src/Signup.js
@@ -1,83 +1,94 @@
-import React from 'react';
-import { View, Text, Touchable, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import Background from './Background';
 import { Arctic, Sky } from './Constants';
 import Field from './Field';
 import Btn from './Btn';
 
-
 const Signup = (props) => {
+  const [userDetails, setUserDetails] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [error, setError] = useState('');
+
+  const saveUserInformation = async (userDetails) => {
+    try {
+      const jsonValue = JSON.stringify(userDetails);
+      await AsyncStorage.setItem('userDetails', jsonValue);
+    } catch (error) {
+      console.error('Error saving user information:', error);
+    }
+  };
+
+  const handleSignup = () => {
+    const { firstName, lastName, email, password, confirmPassword } = userDetails;
+
+    if (!firstName || !lastName || !email || !password || !confirmPassword) {
+      setError('Please fill in all the fields');
+    } else if (password !== confirmPassword) {
+      setError('Passwords do not match');
+    } else {
+      setError('');
+      Alert.alert('Account Created', 'Your account has been successfully created.');
+      // Save the user's information
+      saveUserInformation(userDetails);
+      props.navigation.navigate('Login');
+    }
+  };
+
   return (
     <Background>
-    <View style={{ alignItems:"center", width: 460}}>
-      <Text style={{ 
-        color:"white", 
-      fontSize: 64, 
-      fontWeight: 'bold', 
-      marginTop: 20, 
-      }}>
-      Register
-      </Text>
-      <Text 
-      style={{ 
-        color: "white", 
-        fontSize: 16 
-        }}>
-        Create a new account
+      <View style={{ alignItems: 'center', width: 460 }}>
+        <Text style={{ color: 'white', fontSize: 64, fontWeight: 'bold', marginTop: 20 }}>
+          Register
         </Text>
-      <View 
-      style={{ 
-        backgroundColor: "white", 
-      height: 700, width: 460, 
-      borderTopLeftRadius: 130, 
-      paddingTop: 100, 
-      alignItems: 'center' 
-      }}
-      >
-        <Field placeholder="First Name" />
-        <Field placeholder="Last Name" />
-        <Field placeholder="Email / Username" keyboardType={"email-address"} />
-        <Field placeholder="Password" secureTextEntry={true} />
-        <Field placeholder="Confirm Password" secureTextEntry={true} />
-        <View style={{ display: 'flex', flexDirection: 'row', width: '50%', paddingRight: 16}}>
-          <Text style={{ color: Sky, fontWeight: 'bold', font: 16 }}>
-          By signing in, you agree to our{' '}
-          </Text>
-          <Text style={{ color: 'sky', fontSize: 16}}>
-            Terms & Conditions
-          </Text>
+        <Text style={{ color: 'white', fontSize: 16 }}>Create a new account</Text>
+        <View
+          style={{
+            backgroundColor: 'white',
+            height: 700,
+            width: 460,
+            borderTopLeftRadius: 130,
+            paddingTop: 100,
+            alignItems: 'center',
+          }}>
+          <Field placeholder="First Name" value={userDetails.firstName} onChangeText={(text) => setUserDetails({ ...userDetails, firstName: text })} />
+          <Field placeholder="Last Name" value={userDetails.lastName} onChangeText={(text) => setUserDetails({ ...userDetails, lastName: text })} />
+          <Field placeholder="Email / Username" keyboardType="email-address" value={userDetails.email} onChangeText={(text) => setUserDetails({ ...userDetails, email: text })} />
+          <Field placeholder="Password" secureTextEntry value={userDetails.password} onChangeText={(text) => setUserDetails({ ...userDetails, password: text })} />
+          <Field
+            placeholder="Confirm Password"
+            secureTextEntry
+            value={userDetails.confirmPassword}
+            onChangeText={(text) => setUserDetails({ ...userDetails, confirmPassword: text })}
+          />
+          {error ? <Text style={{ color: 'red' }}>{error}</Text> : null}
+          <View style={{ display: 'flex', flexDirection: 'row', width: '50%', paddingRight: 16 }}>
+            <Text style={{ color: Sky, fontWeight: 'bold', fontSize: 16 }}>
+              By signing in, you agree to our{' '}
+            </Text>
+            <Text style={{ color: 'sky', fontSize: 16 }}>Terms & Conditions</Text>
+          </View>
+          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', width: '78%', paddingRight: 16, marginBottom: 10 }}>
+            <Text style={{ color: 'sky', fontSize: 16 }}>and {''}</Text>
+            <Text style={{ color: Arctic, fontWeight: 'bold', fontSize: 16 }}>Privacy Policy</Text>
+          </View>
+          <Btn textColor="white" bgColor={Arctic} btnLabel="Signup" Press={handleSignup} />
+          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Already have an account ?</Text>
+            <TouchableOpacity onPress={() => props.navigation.navigate('Login')}>
+              <Text style={{ color: Arctic, fontWeight: 'bold', fontSize: 16 }}>Login</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-
-        <View style={{ display: 'flex', flexDirection: 'row', justifyContent: "center", width: '78%', paddingRight: 16, marginBottom: 10}}>
-          <Text style={{color: 'sky', fontSize: 16}}>
-            and {""}
-          </Text>
-          <Text style={{ color: Arctic, fontWeight: 'bold', fontSize: 16}}>
-            Privacy Policy
-          </Text>
-        </View>
-        <Btn 
-        textColor='white' 
-        bgColor={Arctic} 
-        btnLabel="Signup" Press={() => { alert ("Account created"); 
-        props.navigation.navigate('Login')
-        }} 
-        />
-        <View style={{ display: 'flex', flexDirection: 'row', justifyContent:
-        "center"}}>
-          <Text style={{ fontSize: 16, fontWeight: "bold" }}>
-          Already have an account ?
-          </Text>
-          <TouchableOpacity onPress={() => props.navigation.navigate("Login")}>
-          <Text style={{ color: Arctic, fontWeight: 'bold', fontSize: 16 }}>
-          Login
-          </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
       </View>
     </Background>
-  )
-}
+  );
+};
 
 export default Signup;
