02b3e12c9b48b930be0bf3f1daeb34b274ad08f0
Neo <ramoroka.neo@gmail.com>
2023-11-14 10:48:29 +0200

    main

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,45 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Alert } from 'react-native';
+// Import statements
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+  TextInput,
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,
+  Dimensions,
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import Background from './Background';
 import { Arctic, Sky } from './Constants';
-import Field from './Field';
 import Btn from './Btn';
 
 const Login = (props) => {
   const [credentials, setCredentials] = useState({ email: '', password: '' });
   const [error, setError] = useState('');
+  const [storedUserDetails, setStoredUserDetails] = useState(null);
+
+  useEffect(() => {
+    // Fetch user details from AsyncStorage
+    const fetchUserDetails = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem('userDetails');
+        const userDetails = jsonValue != null ? JSON.parse(jsonValue) : null;
+        setStoredUserDetails(userDetails);
+      } catch (error) {
+        console.error('Error fetching user details:', error);
+      }
+    };
+
+    fetchUserDetails();
+  }, []);
 
   const handleLogin = () => {
-    const validEmail = 'user@example.com';
-    const validPassword = 'password';
-
     if (!credentials.email || !credentials.password) {
       setError('Please enter both email and password');
-    } else if (credentials.email === validEmail && credentials.password === validPassword) {
+    } else if (storedUserDetails && credentials.email === storedUserDetails.email && credentials.password === storedUserDetails.password) {
       setError('');
       Alert.alert('Logged In', 'You have successfully logged in.');
       props.navigation.navigate('AudioRecorder');
@@ -26,59 +50,130 @@ const Login = (props) => {
 
   return (
     <Background>
-      <View style={{ alignItems: 'center', width: 460 }}>
-        <Text style={{ color: 'white', fontSize: 64, fontWeight: 'bold', marginVertical: 10 }}>
-          Login
-        </Text>
-        <View
-          style={{
-            backgroundColor: 'white',
-            height: 700,
-            width: 460,
-            borderTopLeftRadius: 130,
-            paddingTop: 100,
-            alignItems: 'center',
-          }}>
-          <Text style={{ fontSize: 40, color: Arctic, fontWeight: 'bold' }}>
-            Welcome Back
-          </Text>
-          <Text style={{ color: 'grey', fontSize: 19, fontWeight: 'bold', marginBottom: 20 }}>
-            Login to your account
-          </Text>
-          <Field
-            placeholder="Email / Username"
-            keyboardType="email-address"
-            value={credentials.email}
-            onChangeText={(text) => setCredentials({ ...credentials, email: text })}
-          />
-          <Field
-            placeholder="Password"
-            secureTextEntry={true}
-            value={credentials.password}
-            onChangeText={(text) => setCredentials({ ...credentials, password: text })}
-          />
-          {error ? <Text style={{ color: 'red' }}>{error}</Text> : null}
-          <View style={{ alignItems: 'flex-end', width: '50%', paddingRight: 16, marginBottom: 100 }}>
-            <Text style={{ color: Sky, fontWeight: 'bold', fontSize: 16 }}>
-              Forgot Password?
-            </Text>
+      <SafeAreaView style={styles.safeAreaView}>
+        <ScrollView contentContainerStyle={styles.scrollViewContent}>
+          <View style={styles.container}>
+            <Text style={styles.title}>Login</Text>
+            <View style={styles.formContainer}>
+              <Text style={styles.welcomeText}>Welcome Back</Text>
+              <Text style={styles.subtitle}>Login to your account</Text>
+              <TextInput
+                style={styles.inputField}
+                placeholder="Email / Username"
+                keyboardType="email-address"
+                value={credentials.email}
+                onChangeText={(text) => setCredentials({ ...credentials, email: text })}
+                placeholderTextColor={Arctic}
+              />
+              <TextInput
+                style={styles.inputField}
+                placeholder="Password"
+                secureTextEntry={true}
+                value={credentials.password}
+                onChangeText={(text) => setCredentials({ ...credentials, password: text })}
+                placeholderTextColor={Arctic}
+              />
+              {error ? <Text style={styles.errorText}>{error}</Text> : null}
+              <TouchableOpacity style={styles.forgotPassword}>
+                <Text style={{ color: Sky, fontWeight: 'bold', fontSize: 16 }}>
+                  Forgot Password?
+                </Text>
+              </TouchableOpacity>
+              <Btn textColor="white" bgColor={Arctic} btnLabel="Login" Press={handleLogin} />
+              <View style={styles.signupContainer}>
+                <Text style={styles.signupText}>Don't have an account? </Text>
+                <TouchableOpacity onPress={() => props.navigation.navigate('Signup')}>
+                  <Text style={styles.signupLink}>Signup</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
           </View>
-          <Btn
-            textColor="white"
-            bgColor={Arctic}
-            btnLabel="Login"
-            Press={handleLogin}
-          />
-          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Don't have an account? </Text>
-            <TouchableOpacity onPress={() => props.navigation.navigate('Signup')}>
-              <Text style={{ color: Arctic, fontWeight: 'bold', fontSize: 16 }}>Signup</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
+        </ScrollView>
+      </SafeAreaView>
     </Background>
   );
 };
+
+const { width, height } = Dimensions.get('window');
+const guidelineBaseWidth = 375;
+const guidelineBaseHeight = 812;
+
+const scale = (size) => (width / guidelineBaseWidth) * size;
+const verticalScale = (size) => (height / guidelineBaseHeight) * size;
+const moderateScale = (size, factor = 0.5) => size + (scale(size) - size) * factor;
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+  container: {
+    alignItems: 'center',
+    width: '100%',
+  },
+  title: {
+    color: 'white',
+    fontSize: moderateScale(54),
+    fontWeight: 'bold',
+    marginVertical: verticalScale(10),
+  },
+  formContainer: {
+    backgroundColor: 'white',
+    height: verticalScale(700),
+    width: width - 1,
+    borderTopLeftRadius: moderateScale(130),
+    paddingTop: verticalScale(60),
+    alignItems: 'center',
+    paddingVertical: verticalScale(30),
+    marginTop: -verticalScale(20),
+  },
+  welcomeText: {
+    fontSize: moderateScale(30),
+    color: Arctic,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    color: 'grey',
+    fontSize: moderateScale(19),
+    fontWeight: 'bold',
+    marginBottom: verticalScale(20),
+  },
+  inputField: {
+    height: verticalScale(40),
+    width: '80%',
+    borderBottomWidth: 1,
+    borderColor: Arctic,
+    color: Arctic,
+    fontSize: moderateScale(16),
+    marginBottom: verticalScale(16),
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: verticalScale(10),
+  },
+  forgotPassword: {
+    alignItems: 'flex-end',
+    width: '50%',
+    paddingRight: moderateScale(16),
+    marginBottom: verticalScale(100),
+  },
+  signupContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: verticalScale(20),
+  },
+  signupText: {
+    fontSize: moderateScale(16),
+    fontWeight: 'bold',
+  },
+  signupLink: {
+    color: Arctic,
+    fontWeight: 'bold',
+    fontSize: moderateScale(16),
+    marginLeft: moderateScale(5),
+  },
+});
 
 export default Login;
--- src/Signup.js
+++ src/Signup.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Alert } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+  ScrollView,
+  Dimensions,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Background from './Background';
 import { Arctic, Sky } from './Constants';
-import Field from './Field';
 import Btn from './Btn';
 
 const Signup = (props) => {
@@ -43,52 +52,160 @@ const Signup = (props) => {
 
   return (
     <Background>
-      <View style={{ alignItems: 'center', width: 460 }}>
-        <Text style={{ color: 'white', fontSize: 64, fontWeight: 'bold', marginTop: 20 }}>
-          Register
-        </Text>
-        <Text style={{ color: 'white', fontSize: 16 }}>Create a new account</Text>
-        <View
-          style={{
-            backgroundColor: 'white',
-            height: 700,
-            width: 460,
-            borderTopLeftRadius: 130,
-            paddingTop: 100,
-            alignItems: 'center',
-          }}>
-          <Field placeholder="First Name" value={userDetails.firstName} onChangeText={(text) => setUserDetails({ ...userDetails, firstName: text })} />
-          <Field placeholder="Last Name" value={userDetails.lastName} onChangeText={(text) => setUserDetails({ ...userDetails, lastName: text })} />
-          <Field placeholder="Email / Username" keyboardType="email-address" value={userDetails.email} onChangeText={(text) => setUserDetails({ ...userDetails, email: text })} />
-          <Field placeholder="Password" secureTextEntry value={userDetails.password} onChangeText={(text) => setUserDetails({ ...userDetails, password: text })} />
-          <Field
-            placeholder="Confirm Password"
-            secureTextEntry
-            value={userDetails.confirmPassword}
-            onChangeText={(text) => setUserDetails({ ...userDetails, confirmPassword: text })}
-          />
-          {error ? <Text style={{ color: 'red' }}>{error}</Text> : null}
-          <View style={{ display: 'flex', flexDirection: 'row', width: '50%', paddingRight: 16 }}>
-            <Text style={{ color: Sky, fontWeight: 'bold', fontSize: 16 }}>
-              By signing in, you agree to our{' '}
-            </Text>
-            <Text style={{ color: 'sky', fontSize: 16 }}>Terms & Conditions</Text>
+      <SafeAreaView style={styles.safeAreaView}>
+        <ScrollView contentContainerStyle={styles.scrollViewContent}>
+          <View style={styles.container}>
+            <View style={styles.header}>
+              <Text style={styles.headerText}>Register</Text>
+              <Text style={styles.subHeaderText}>Create a new account</Text>
+            </View>
+            <View style={styles.formContainer}>
+              <TextInput
+                style={styles.inputField}
+                placeholder="First Name"
+                value={userDetails.firstName}
+                onChangeText={(text) => setUserDetails({ ...userDetails, firstName: text })}
+                placeholderTextColor={Arctic}
+              />
+              <TextInput
+                style={styles.inputField}
+                placeholder="Last Name"
+                value={userDetails.lastName}
+                onChangeText={(text) => setUserDetails({ ...userDetails, lastName: text })}
+                placeholderTextColor={Arctic}
+              />
+              <TextInput
+                style={styles.inputField}
+                placeholder="Email / Username"
+                keyboardType="email-address"
+                value={userDetails.email}
+                onChangeText={(text) => setUserDetails({ ...userDetails, email: text })}
+                placeholderTextColor={Arctic}
+              />
+              <TextInput
+                style={styles.inputField}
+                placeholder="Password"
+                secureTextEntry
+                value={userDetails.password}
+                onChangeText={(text) => setUserDetails({ ...userDetails, password: text })}
+                placeholderTextColor={Arctic}
+              />
+              <TextInput
+                style={styles.inputField}
+                placeholder="Confirm Password"
+                secureTextEntry
+                value={userDetails.confirmPassword}
+                onChangeText={(text) => setUserDetails({ ...userDetails, confirmPassword: text })}
+                placeholderTextColor={Arctic}
+              />
+              {error ? <Text style={styles.errorText}>{error}</Text> : null}
+              <View style={styles.termsContainer}>
+                <Text style={styles.termsText}>
+                  By signing in, you agree to our{' '}
+                  <Text style={{ color: Sky, fontWeight: 'bold' }}>Terms & Conditions</Text>
+                </Text>
+                <Text style={{ color: 'sky' }}> and </Text>
+                <Text style={styles.termsText}>Privacy Policy</Text>
+              </View>
+              <Btn textColor="white" bgColor={Arctic} btnLabel="Signup" Press={handleSignup} />
+              <View style={styles.loginContainer}>
+                <Text style={styles.loginText}>Already have an account?</Text>
+                <TouchableOpacity onPress={() => props.navigation.navigate('Login')}>
+                  <Text style={styles.loginLink}>Login</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
           </View>
-          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', width: '78%', paddingRight: 16, marginBottom: 10 }}>
-            <Text style={{ color: 'sky', fontSize: 16 }}>and {''}</Text>
-            <Text style={{ color: Arctic, fontWeight: 'bold', fontSize: 16 }}>Privacy Policy</Text>
-          </View>
-          <Btn textColor="white" bgColor={Arctic} btnLabel="Signup" Press={handleSignup} />
-          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Already have an account ?</Text>
-            <TouchableOpacity onPress={() => props.navigation.navigate('Login')}>
-              <Text style={{ color: Arctic, fontWeight: 'bold', fontSize: 16 }}>Login</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
+        </ScrollView>
+      </SafeAreaView>
     </Background>
   );
 };
+
+const { width, height } = Dimensions.get('window');
+const guidelineBaseWidth = 375;
+const guidelineBaseHeight = 812;
+
+const scale = (size) => (width / guidelineBaseWidth) * size;
+const verticalScale = (size) => (height / guidelineBaseHeight) * size;
+const moderateScale = (size, factor = 0.5) => size + (scale(size) - size) * factor;
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+  container: {
+    alignItems: 'center',
+    width: '100%',
+  },
+  header: {
+    alignItems: 'center',
+    marginTop: verticalScale(20),
+  },
+  headerText: {
+    color: 'white',
+    fontSize: moderateScale(54),
+    fontWeight: 'bold',
+  },
+  subHeaderText: {
+    color: 'white',
+    fontSize: moderateScale(16),
+  },
+  formContainer: {
+    backgroundColor: 'white',
+    height: verticalScale(710),
+    width: width - 1,
+    borderTopLeftRadius: moderateScale(130),
+    paddingTop: verticalScale(60),
+    alignItems: 'center',
+    paddingVertical: verticalScale(30),
+    marginTop: -verticalScale(20),
+  },
+  inputField: {
+    height: verticalScale(40),
+    width: '80%',
+    borderBottomWidth: 1,
+    borderColor: Arctic,
+    color: Arctic,
+    fontSize: moderateScale(16),
+    marginBottom: verticalScale(16),
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: verticalScale(10),
+  },
+  termsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '90%',
+    paddingRight: moderateScale(65),
+    marginTop: verticalScale(5),
+    marginBottom: verticalScale(10),
+  },
+  termsText: {
+    color: Arctic,
+    fontWeight: 'bold',
+    fontSize: moderateScale(16),
+  },
+  loginContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: verticalScale(20),
+  },
+  loginText: {
+    fontSize: moderateScale(16),
+    fontWeight: 'bold',
+  },
+  loginLink: {
+    color: Arctic,
+    fontWeight: 'bold',
+    fontSize: moderateScale(16),
+    marginLeft: moderateScale(5),
+  },
+});
 
 export default Signup;
